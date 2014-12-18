1a51938902c4147c648dc6d987399c82c9536207
zeld4 <radhoane.ben@gmail.com>
2014-12-18 13:38:56 +0100

    Delete

--- mainTable.js
+++ mainTable.js
@@ -5,22 +5,21 @@ define(['js/domReady',"mainThree"], function(domReady,mainThree) {
 			server.on('client1T', function (msg) {
 				console.log("Receivedfrom1:"+msg)
 			});
+                  
                         // Desactiver juste pour tester la commande vocale
-                        
 			server.on('rotation',function(msg) {
 				console.log("Rotation:"+JSON.stringify(msg));
 				
 				if(document.getElementById("axeX").checked){
                                     pass;
-                                    //mainThree.object.rotation.x = msg.g; //avant-arriere
+                                    mainThree.object.rotation.x = msg.g; //avant-arriere
 				}
 				if(document.getElementById("axeY").checked){
-                                    pass;
-                                    //mainThree.object.rotation.y = msg.a; //gauche-droite
+                                    mainThree.object.rotation.y = msg.a; //gauche-droite
 				}
 				if(document.getElementById("axeZ").checked){
                                     pass;
-        					//mainThree.object.rotation.z = msg.b; //aiguilles d'une montre
+        				mainThree.object.rotation.z = msg.b; //aiguilles d'une montre
 				}
 			});
                         
@@ -32,8 +31,10 @@ define(['js/domReady',"mainThree"], function(domReady,mainThree) {
                             }
                             if (msg.a = "delete"){
                             
-                                mainThree.object.rotation.x += 0.1;
+                                mainThree.object.visible = false;
                                 }
+                            if (msg.a = "")
+                                pass;
                             })
                             
 		//});
